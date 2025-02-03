052b5a9d76d2d8925b6aedb4d4235ed04f193c52
Ray <mr.raywilliams01@gmail.com>
2025-02-03 01:49:19 -0500

    Initial Commit

---
+++ MobileTakeHome/MobileTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTakeHomeApp.swift
+//  MobileTakeHome
+//
+//  Created by ray williams on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
